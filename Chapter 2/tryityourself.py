f5f1f3be7b428f14d6397666475405d3a114fdc5
Mary  Gore <marygore@Marys-Air.attlocal.net>
2022-02-11 08:39:07 -0600

    refactored

---
+++ Chapter 2/tryityourself.py
@@ -0,0 +1,53 @@
+#Chapter 2 
+##Try it yourself
+
+#2.1 
+message = 'Mary is so cool'
+print(message)
+
+#2.2
+message = "She's learning how to code"
+print(message)
+
+#2.3
+name = 'Mary'
+print(f"Hello {name}, you're @cornjolio!")
+
+#2.4
+name = 'Mary Gore'
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+#2.5
+print('Rick Sanchez once said, "Wubba lubba dub dub"')
+print('which translates, "I am in great pain, please help me."')
+
+#2.6
+famous_person = 'Rick Sanchez'
+msg = f'{famous_person} once said, "Wubba lubba dub dub. which translates, "I am in great pain, please help me."'
+print(msg)
+
+#2.7
+name = ' Mary Gore '
+print("\nUsing rstrip:")
+print(name.rstrip())
+print("\nUsing lstrip:")
+print(name.lstrip())
+print("\nUsing strip:")
+print(name.strip())
+
+#2.8
+print(4+4)
+print(16-8)
+print(4*2)
+print(16/2)
+
+#2.9
+favorite_number = 3
+print(f"My favorite number is {favorite_number}")
+
+#2.10 Hello, I'm commenting
+
+#2.11 The Zen Of Python
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python
+## Python Crash Course
+Here are some projects that I created while reading [Python Crash Course, 2nd Edition](https://nostarch.com) by Eric Matthes.
